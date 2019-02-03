e3827dc7802e47325c7284e4fb74a636e7cc9a93
Zianii <41494410+Zianii@users.noreply.github.com>
2019-02-03 21:20:06 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -150,44 +150,6 @@ msg.delete();
 
 
 
-
-
-client.on('message', message => {
-  var prefix ="-";
-if(message.content.startsWith(prefix +"server")){
-if(!message.channel.guild) return message.reply(' ');
-const millis = new Date().getTime() - message.guild.createdAt.getTime();
-const now = new Date();
-dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
-const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
-const days = millis / 1000 / 60 / 60 / 24;
-let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
-var embed  = new Discord.RichEmbed()
-.setAuthor(message.guild.name, message.guild.iconURL)
-.addField("**🆔 Server ID:**", message.guild.id,true)
-.addField("**📅 Created On**", message.guild.createdAt.toLocaleString(),true)
-.addField("**👑 Owned by**",`${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`)
-.addField("👥 Members ",`[${message.guild.memberCount}]`,true)
-.addField('**💬 Channels **',`**${message.guild.channels.filter(m => m.type === 'text').size}**` + ' text | Voice  '+ `**${message.guild.channels.filter(m => m.type === 'voice').size}** `,true)
-.addField("**🌍 Others **" , message.guild.region,true)
-.addField("** 🔐 Roles **",`**[${message.guild.roles.size}]** Role `,true)
-.setColor('#000000')
-message.channel.sendEmbed(embed)
-
-} /////// Galal ///////// Mal ,  Codes
-});
-
-
-
-
-
-
-
-
-
-
-
-
 //! KinG66S.❤#0045
 var KinG66S = {};//! KinG66S.❤#0045
 client.on('guildMemberRemove', member => {//! KinG66S.❤#0045
@@ -553,10 +515,6 @@ gg.send({embed : new Discord.RichEmbed()
 	
 	
 	
-
-	
-
-
 
 
 
